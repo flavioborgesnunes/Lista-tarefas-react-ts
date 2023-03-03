e03a2d50c8d4d57b9b310ea874600aa88ecf2151
Flavio Borges Nunes <flavioborgesnunes@gmail.com>
2023-03-03 15:30:22 -0300

    Ajustes h1

--- src/components/lista/index.tsx
+++ src/components/lista/index.tsx
@@ -11,7 +11,7 @@ interface Props{
 function Lista ({tarefas, selecionaTarefa}: Props){
     return(
         <aside className={style.listaTarefas}>
-            <h2>Tarefas do Bem :</h2>
+            <h2>Tarefas :</h2>
             <ul>
                 {tarefas.map(item=> (
                     <Item
